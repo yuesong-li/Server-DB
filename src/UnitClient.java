68b5747d52b3ee95d55f7e2d89f683fd5d0b4f0a
rahstafarian <m.olsson@mail.com>
2012-11-04 10:48:36 +0100

    Update src/UnitClient.java

--- src/UnitClient.java
+++ src/UnitClient.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,37 +10,68 @@ import java.net.Socket;
 public class UnitClient {
 
     static int portnumber = 8888;
-    static PrintWriter pw = null;
-    static Socket client = null;
-    static OutputStream clientOut = null;
-    static InputStream clientIn = null;
-    static BufferedReader br = null;
+	static PrintWriter pw = null;
+	static Socket client = null;
+	static OutputStream clientOut = null;
+	static InputStream clientIn = null;
+	static BufferedReader br = null;
+	static int count = 0;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        try {
-            client = new Socket(InetAddress.getLocalHost(), portnumber);
-            System.out.println("Client socket is created "
-                    + client.getRemoteSocketAddress());
-            clientOut = client.getOutputStream();
-            pw = new PrintWriter(clientOut, true);
-            clientIn = client.getInputStream();
-            br = new BufferedReader(new InputStreamReader(clientIn));
+		try {
+			client = new Socket(InetAddress.getLocalHost(), portnumber);
+			System.out.println("Client socket is created "
+					+ client.getRemoteSocketAddress());
+			clientOut = client.getOutputStream();
+			pw = new PrintWriter(clientOut, true);
+			clientIn = client.getInputStream();
+			br = new BufferedReader(new InputStreamReader(clientIn));
+			sendUserPass();
+			receiveStartupDetails();
+			sendToServer();
 
-            sendToServer();
+			while (true) {
+				if (br.ready()) {
+					String deviceAnswer = br.readLine();
+					System.out.println("Unit received message : " + deviceAnswer);	
+				} else {
+					/*
+					 * Code for running other unit specific code. 
+					 * Will only be invoked when the bufferedReader doesnt have anything to read.
+					 */
+				}
+			}
 
-            while (true) {
-                String deviceAnswer = br.readLine();
-                System.out.println("Unit received message : " + deviceAnswer);
-            }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	}
 
-    }
-
-    private static void sendToServer() {
-        pw.println("light:on");
-    }
+	private static void sendToServer() {
+		System.out.println("Sending command to server : light:On");
+		pw.println("light:On");
+	}
+	
+	private static void receiveStartupDetails() {
+		try {
+			String startup = br.readLine();
+			System.out.println("Client knows about following device : " + startup);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void sendUserPass() {
+		String sendUser = "username";
+		String sendPass = "password";
+		pw.println(sendUser+":"+sendPass);
+		try {
+			String answer = br.readLine();
+			System.out.println("Result of validation : " + answer);
+		} catch (IOException e) {
+			System.out.println("Method:sendUserPass : " + e.getMessage());
+		}
+	}
 }
